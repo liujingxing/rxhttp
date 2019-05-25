0433c0ea6c6fc246300147d9ff253a02a3fb637e
liujingxing <>
2019-05-25 21:32:17 +0800

    目录结构调整

--- app/src/main/java/com/example/httpsender/parser/DataListParser.java
+++ app/src/main/java/com/example/httpsender/parser/DataListParser.java
@@ -1,9 +1,12 @@
-package com.example.httpsender;
+package com.example.httpsender.parser;
 
 
+
+import com.example.httpsender.entity.Data;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.List;
 
 import rxhttp.wrapper.annotation.Parser;
 import rxhttp.wrapper.entity.ParameterizedTypeImpl;
@@ -12,38 +15,38 @@ import rxhttp.wrapper.parse.AbstractParser;
 import rxhttp.wrapper.utils.GsonUtil;
 
 /**
- * Data<PageList<T>> 数据解析器,解析完成对Data对象做判断,如果ok,返回数据 PageList<T>
+ * Data<List<T>> 数据解析器,解析完成对Data对象做判断,如果ok,返回数据 List<T>
  * User: ljx
  * Date: 2018/10/23
  * Time: 13:49
  */
-@Parser(name = "DataPageListParser")
-public class DataPageListParser<T> extends AbstractParser<PageList<T>> {
+@Parser(name = "DataListParser")
+public class DataListParser<T> extends AbstractParser<List<T>> {
 
-    protected DataPageListParser() {
+    protected DataListParser() {
         super();
     }
 
-    private DataPageListParser(Type type) {
+    private DataListParser(Type type) {
         super(type);
     }
 
-    public static <T> DataPageListParser<T> get(Class<T> type) {
-        return new DataPageListParser<>(type);
+    public static <T> DataListParser<T> get(Class<T> type) {
+        return new DataListParser<>(type);
     }
 
     @Override
-    public PageList<T> onParse(okhttp3.Response response) throws IOException {
+    public List<T> onParse(okhttp3.Response response) throws IOException {
         String content = getResult(response); //从Response中取出Http执行结果
-        final Type type = ParameterizedTypeImpl.get(Data.class, PageList.class, mType); //获取泛型类型
-        Data<PageList<T>> data = GsonUtil.getObject(content, type);
+        final Type type = ParameterizedTypeImpl.get(Data.class, List.class, mType); //获取泛型类型
+        Data<List<T>> data = GsonUtil.getObject(content, type);
         if (data == null) //为空 ，表明数据不正确
             throw new ParseException("data parse error");
         //跟服务端协议好，code等于100，才代表数据正确,否则，抛出异常
         if (data.getCode() != 100) {
             throw new ParseException(String.valueOf(data.getCode()), data.getMsg());
         }
-        PageList<T> t = data.getData(); //获取data字段
+        List<T> t = data.getData(); //获取data字段
         if (t == null) {  //为空，有可能是参数错误或者其他原因，导致服务器不能正确给我们data字段数据
             throw new ParseException(String.valueOf(data.getCode()), data.getMsg());
         }
