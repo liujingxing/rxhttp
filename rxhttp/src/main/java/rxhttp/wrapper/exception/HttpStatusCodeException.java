5987cf5e88feedc1b6580be2e9c8ce684710fbf5
liujingxing <18268800083@163.com>
2019-06-09 12:42:10 +0800

    完善日志打印

--- rxhttp/src/main/java/rxhttp/wrapper/exception/HttpStatusCodeException.java
+++ rxhttp/src/main/java/rxhttp/wrapper/exception/HttpStatusCodeException.java
@@ -6,44 +6,31 @@ import io.reactivex.annotations.Nullable;
 import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 import rxhttp.wrapper.utils.LogUtil;
 
 /**
- * Http 状态码 小于200或者大于等于300时 (code < 200 || code >= 300)，抛出此异常
+ * Http 状态码 小于200或者大于等于300时 (code < 200 || code >= 300),或者ResponseBody等于null，抛出此异常
  * <p>
  * 可通过{@link #getLocalizedMessage()}方法获取code
  * User: ljx
  * Date: 2019-06-09
  * Time: 09:56
  */
-public class HttpStatusCodeException extends IOException {
+public final class HttpStatusCodeException extends IOException {
 
     private String statusCode; //Http响应状态吗
     private String requestMethod; //请求方法，Get/Post等
     private String requestUrl; //请求Url
     private Headers responseHeaders; //响应头
-    private String responseResult; //响应结果
 
-    public HttpStatusCodeException(Response response) {
+    HttpStatusCodeException(Response response) {
         super(response.message());
         statusCode = String.valueOf(response.code());
         Request request = response.request();
         requestMethod = request.method();
         requestUrl = request.url().toString();
         responseHeaders = response.headers();
-        responseResult = getResponseResult(response);
         LogUtil.log(this);
-    }
-
-    private String getResponseResult(Response response) {
-        ResponseBody body = response.body();
-        if (body == null) return "ResponseBody is null";
-        try {
-            return body.string();
-        } catch (IOException e) {
-            return e.getMessage();
-        }
     }
 
     @Nullable
@@ -68,10 +55,6 @@ public class HttpStatusCodeException extends IOException {
         return responseHeaders;
     }
 
-    public String getResponseResult() {
-        return responseResult;
-    }
-
     @Override
     public String toString() {
         return "-------------------" +
@@ -79,6 +62,6 @@ public class HttpStatusCodeException extends IOException {
             " Code=" + statusCode + " HttpStatusCodeException -------------------" +
             "\nUrl = " + requestUrl +
             "\n\nHeaders = " + responseHeaders +
-            "\nResult = " + responseResult;
+            "\nMessage = " + getMessage();
     }
 }
