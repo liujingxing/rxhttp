838662861de920079f74225f82f4b9429f14fcbe
liujingxing <>
2019-11-16 18:02:52 +0800

    1、NoBodyParam、FormParam支持对参数的增删改查 2、NoBodyParam、FormParam支持一个key添加多个参数 3、NoBodyParam、FormParam新增addEncoded(String,String)方法添加已经编码的参数

--- rxhttp/src/main/java/rxhttp/wrapper/param/NoBodyParam.java
+++ rxhttp/src/main/java/rxhttp/wrapper/param/NoBodyParam.java
@@ -1,11 +1,15 @@
 package rxhttp.wrapper.param;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import io.reactivex.annotations.NonNull;
+import io.reactivex.annotations.Nullable;
 import okhttp3.HttpUrl;
 import okhttp3.RequestBody;
+import rxhttp.wrapper.entity.KeyValuePair;
 import rxhttp.wrapper.utils.BuildUtil;
 
 /**
@@ -16,7 +20,8 @@ import rxhttp.wrapper.utils.BuildUtil;
  */
 public class NoBodyParam extends AbstractParam<NoBodyParam> {
 
-    private Map<String, Object> mParam; //请求参数
+    private List<KeyValuePair> mKeyValuePairs; //键值对数组
+
     /**
      * @param url    请求路径
      * @param method {@link Method#GET,Method#HEAD,Method#DELETE}
@@ -32,28 +37,82 @@ public class NoBodyParam extends AbstractParam<NoBodyParam> {
 
     @Override
     public HttpUrl getHttpUrl() {
-        return BuildUtil.getHttpUrl(getSimpleUrl(), mParam);
+        return BuildUtil.getHttpUrl(getSimpleUrl(), mKeyValuePairs);
     }
 
     @Override
     public NoBodyParam add(String key, Object value) {
-        if (value == null) value = "";
-        Map<String, Object> param = mParam;
-        if (param == null) {
-            param = mParam = new LinkedHashMap<>();
+        return add(new KeyValuePair(key, value));
+    }
+
+    public NoBodyParam addEncoded(String key, Object value) {
+        return add(new KeyValuePair(key, value, true));
+    }
+
+    public NoBodyParam removeAllBody(String key) {
+        final List<KeyValuePair> keyValuePairs = mKeyValuePairs;
+        if (keyValuePairs == null) return this;
+        Iterator<KeyValuePair> iterator = keyValuePairs.iterator();
+        while (iterator.hasNext()) {
+            KeyValuePair next = iterator.next();
+            if (next.equals(key))
+                iterator.remove();
         }
-        param.put(key, value);
+        return this;
+    }
+
+    public NoBodyParam removeAllBody() {
+        final List<KeyValuePair> keyValuePairs = mKeyValuePairs;
+        if (keyValuePairs != null)
+            keyValuePairs.clear();
+        return this;
+    }
+
+    public NoBodyParam set(String key, Object value) {
+        removeAllBody(key);
+        return add(key, value);
+    }
+
+    public NoBodyParam setEncoded(String key, Object value) {
+        removeAllBody(key);
+        return addEncoded(key, value);
+    }
+
+    @Nullable
+    public Object queryValue(String key) {
+        final List<KeyValuePair> keyValuePairs = mKeyValuePairs;
+        if (keyValuePairs == null) return this;
+        for (KeyValuePair pair : keyValuePairs) {
+            if (pair.equals(key))
+                return pair.getValue();
+        }
+        return null;
+    }
+
+    @NonNull
+    public List<Object> queryValues(String key) {
+        final List<KeyValuePair> keyValuePairs = mKeyValuePairs;
+        if (keyValuePairs == null) return Collections.emptyList();
+        List<Object> values = new ArrayList<>();
+        for (KeyValuePair pair : keyValuePairs) {
+            if (pair.equals(key))
+                values.add(pair.getValue());
+        }
+        return Collections.unmodifiableList(values);
+    }
+
+    private NoBodyParam add(KeyValuePair keyValuePair) {
+        List<KeyValuePair> keyValuePairs = mKeyValuePairs;
+        if (keyValuePairs == null) {
+            keyValuePairs = mKeyValuePairs = new ArrayList<>();
+        }
+        keyValuePairs.add(keyValuePair);
         return this;
     }
 
     @Override
     public final RequestBody getRequestBody() {
         return null;
-    }
-
-    @NonNull
-    public Map<String, Object> getParams() {
-        return mParam;
     }
 
     @Override
