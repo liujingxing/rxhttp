d34c8397fc192f4376ade03d3341d9973f5ae247
“liujingxing” <18268800083ljx@gmail.com>
2019-11-18 15:26:57 +0800

    更新demo

---
+++ app/src/main/java/com/example/httpsender/entity/Location.java
@@ -0,0 +1,17 @@
+package com.example.httpsender.entity;
+
+/**
+ * User: ljx
+ * Date: 2019-11-18
+ * Time: 14:51
+ */
+public class Location {
+
+    private double longitude;
+    private double latitude;
+
+    public Location(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
