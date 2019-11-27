a1e79178dba7f7290a01f0e2c6bec1b2b7d0c9b3
liujingxing <18268800083ljx@gmail.com>
2019-11-27 12:41:08 +0800

    增加Xml、FastJson转换器案例

---
+++ app/src/main/java/com/example/httpsender/entity/NewsXml.java
@@ -0,0 +1,17 @@
+package com.example.httpsender.entity;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
+/**
+ * User: ljx
+ * Date: 2019-11-22
+ * Time: 23:34
+ */
+@Root(name = "route", strict = false) //要解析的xml数据的头部
+public class NewsXml {
+    @Attribute
+    public String tag;
+    @Attribute
+    public String title;
+}
