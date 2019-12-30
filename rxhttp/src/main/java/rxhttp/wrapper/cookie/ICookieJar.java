f15baec0f46d5b4a13cfcc5f4730ad2d3d6626e3
liujingxing <18268800083@163.com>
2019-12-30 11:37:54 +0800

    增加cookie管理类，CookieStore ，支持内存/磁盘缓存

---
+++ rxhttp/src/main/java/rxhttp/wrapper/cookie/ICookieJar.java
@@ -0,0 +1,44 @@
+package rxhttp.wrapper.cookie;
+
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+/**
+ * User: ljx
+ * Date: 2019-12-29
+ * Time: 20:47
+ */
+public interface ICookieJar extends CookieJar {
+
+    @Override
+    default void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+        saveCookie(url, cookies);
+    }
+
+    @Override
+    default List<Cookie> loadForRequest(HttpUrl url) {
+        return loadCookie(url);
+    }
+
+    /**
+     * 保存url对应所有cookie
+     */
+    void saveCookie(HttpUrl url, List<Cookie> cookies);
+
+    /**
+     * 保存url对应所有cookie
+     */
+    void saveCookie(HttpUrl url, Cookie cookie);
+
+    /**
+     * 加载url所有的cookie
+     */
+    List<Cookie> loadCookie(HttpUrl url);
+
+    void removeCookie(HttpUrl url);
+
+    void removeAllCookie();
+}
