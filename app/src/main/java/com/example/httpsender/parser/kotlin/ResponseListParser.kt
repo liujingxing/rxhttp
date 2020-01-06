50adaad48b55a41fcfd19e82eaee2adfd764597f
liujingxing <18268800083ljx@gmail.com>
2020-01-06 18:00:08 +0800

    泛型类型更改

--- app/src/main/java/com/example/httpsender/parser/kotlin/ResponseListParser.kt
+++ app/src/main/java/com/example/httpsender/parser/kotlin/ResponseListParser.kt
@@ -14,16 +14,16 @@ import java.io.IOException
  * Time: 13:49
  */
 //@Parser(name = "ResponseList")
-class ResponseListParser<T> : AbstractParser<List<T>> {
+class ResponseListParser<T> : AbstractParser<MutableList<T>> {
 
     protected constructor() : super()
 
     constructor(type: Class<T>) : super(type)
 
     @Throws(IOException::class)
-    override fun onParse(response: okhttp3.Response): List<T> {
-        val type = ParameterizedTypeImpl.get(Response::class.java, List::class.java, mType) //获取泛型类型
-        val data = convert<Response<List<T>>>(response, type)
+    override fun onParse(response: okhttp3.Response): MutableList<T> {
+        val type = ParameterizedTypeImpl.get(Response::class.java, MutableList::class.java, mType) //获取泛型类型
+        val data = convert<Response<MutableList<T>>>(response, type)
         val list = data.data //获取data字段
         if (data.code != 0 || list == null) {  //code不等于0，说明数据不正确，抛出异常
             throw ParseException(data.code.toString(), data.msg, response)
