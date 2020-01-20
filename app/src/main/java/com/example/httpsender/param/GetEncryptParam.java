7ea5b28797b7dc9dd6fb76954008927b394b0053
liujingxing <18268800083ljx@gmail.com>
2020-01-20 19:47:09 +0800

    添加 get加密请求示例代码

--- app/src/main/java/com/example/httpsender/param/GetEncryptParam.java
+++ app/src/main/java/com/example/httpsender/param/GetEncryptParam.java
@@ -1,10 +1,12 @@
 package com.example.httpsender.param;
 
+import okhttp3.HttpUrl;
 import rxhttp.wrapper.annotation.Param;
 import rxhttp.wrapper.param.Method;
 import rxhttp.wrapper.param.NoBodyParam;
 
 /**
+ * 加密get请求
  * User: ljx
  * Date: 2019-09-12
  * Time: 17:25
@@ -16,7 +18,25 @@ public class GetEncryptParam extends NoBodyParam {
         super(url, Method.GET);
     }
 
-    public void test(){
+    public void test() {
 
+    }
+
+    @Override
+    public HttpUrl getHttpUrl() {
+        HttpUrl httpUrl = super.getHttpUrl();
+        int querySize = httpUrl.querySize();
+        StringBuilder paramsBuilder = new StringBuilder(); //存储加密后的参数
+        for (int i = 0; i < querySize; i++) {
+            //这里遍历所有添加的参数，可对参数进行加密操作
+            String key = httpUrl.queryParameterName(i);
+            String value = httpUrl.queryParameterValue(i);
+
+            //加密逻辑自己写
+
+        }
+        String simpleUrl = getSimpleUrl();  //拿到请求Url
+        if (paramsBuilder.length() == 0) return HttpUrl.get(simpleUrl);
+        return HttpUrl.get(simpleUrl + "?" + paramsBuilder);  //将加密后的参数和url组拼成HttpUrl对象并返回
     }
 }
