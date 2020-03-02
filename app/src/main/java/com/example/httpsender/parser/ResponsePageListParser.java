72ccecf01f57a4338cc1b2c7fbbe45454426e549
liujingxing <18268800083@163.com>
2020-03-02 15:40:34 +0800

    增加注释

--- app/src/main/java/com/example/httpsender/parser/ResponsePageListParser.java
+++ app/src/main/java/com/example/httpsender/parser/ResponsePageListParser.java
@@ -13,7 +13,7 @@ import rxhttp.wrapper.exception.ParseException;
 import rxhttp.wrapper.parse.AbstractParser;
 
 /**
- * Response<PageList<T>> 数据解析器,解析完成对Response对象做判断,如果ok,返回数据 PageList<T>
+ * 输入T，输出PageList<T>，并对code统一判断
  * User: ljx
  * Date: 2018/10/23
  * Time: 13:49
