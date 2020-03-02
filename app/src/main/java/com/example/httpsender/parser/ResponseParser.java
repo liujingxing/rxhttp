dbc40a95310b204f2e0e0e7ad0206cfd79e5d4ab
liujingxing <18268800083ljx@gmail.com>
2020-03-02 15:40:34 +0800

    增加注释

--- app/src/main/java/com/example/httpsender/parser/ResponseParser.java
+++ app/src/main/java/com/example/httpsender/parser/ResponseParser.java
@@ -12,7 +12,7 @@ import rxhttp.wrapper.exception.ParseException;
 import rxhttp.wrapper.parse.AbstractParser;
 
 /**
- * Response<T> 数据解析器,解析完成对Response对象做判断,如果ok,返回数据 T
+ * 输入T,输出T,并对code统一判断
  * User: ljx
  * Date: 2018/10/23
  * Time: 13:49
