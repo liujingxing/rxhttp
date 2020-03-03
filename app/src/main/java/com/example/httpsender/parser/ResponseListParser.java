4ede93a7ee0945deeb474175706cff3a0ce49fad
liujingxing <18268800083@163.com>
2020-03-03 14:40:17 +0800

    增加解析器两个构造方法的注释

--- app/src/main/java/com/example/httpsender/parser/ResponseListParser.java
+++ app/src/main/java/com/example/httpsender/parser/ResponseListParser.java
@@ -21,11 +21,26 @@ import rxhttp.wrapper.parse.AbstractParser;
 @Parser(name = "ResponseList")
 public class ResponseListParser<T> extends AbstractParser<List<T>> {
 
-    //注意：此构造方法一定要用protected关键字修饰，否则调用此构造方法将拿不到泛型类型
+    /**
+     * 此构造方法适用于任意Class对象，但更多用于带泛型的Class对象，如：List<Student>
+     *
+     * 用法:
+     * Java: .asParser(new ResponseListParser<List<Student>>(){})
+     * Kotlin: .asParser(object : ResponseListParser<List<Student>>() {})
+     *
+     * 注：此构造方法一定要用protected关键字修饰，否则调用此构造方法将拿不到泛型类型
+     */
     protected ResponseListParser() {
         super();
     }
 
+    /**
+     * 此构造方法仅适用于解析不带泛型的Class对象，如: Student.class
+     *
+     * 用法
+     * Java: .asParser(new ResponseListParser<>(Student.class))   或者  .asResponseList(Student.class)
+     * Kotlin: .asParser(ResponseListParser(Student::class.java)) 或者  .asResponseList(Student::class.java)
+     */
     public ResponseListParser(Class<T> type) {
         super(type);
     }
