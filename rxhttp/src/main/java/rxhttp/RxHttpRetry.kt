a94a2952e7146bc4d0ff383e89e68584da51ac4f
liujingxing <18268800083ljx@gmail.com>
2020-03-21 19:27:17 +0800

    kotlin环境下，新增timeout方法，用于为单个请求设置超时时长

--- rxhttp/src/main/java/rxhttp/RxHttpRetry.kt
+++ rxhttp/src/main/java/rxhttp/RxHttpRetry.kt
@@ -1,22 +1,44 @@
 package rxhttp
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
 import okhttp3.Headers
 import okhttp3.Request
-import rxhttp.wrapper.parse.*
+import rxhttp.wrapper.parse.BitmapParser
+import rxhttp.wrapper.parse.OkResponseParser
+import rxhttp.wrapper.parse.Parser
+import rxhttp.wrapper.parse.SimpleParser
 
 /**
- * 失败重试
+ * 失败重试/超时处理
  * User: ljx
  * Date: 2020/3/21
  * Time: 17:06
  */
 class RxHttpRetry(
-    val baseRxHttp: BaseRxHttp,
-    var times: Int = Int.MAX_VALUE,
-    val period: Long = 0,
-    val test: ((Throwable) -> Boolean)? = null
-)
+    internal val baseRxHttp: BaseRxHttp
+) {
+    internal var times = 0
+    internal var period = 0L
+    internal var test: ((Throwable) -> Boolean)? = null
+    internal var timeoutMillis = 0L
+
+    fun timeout(timeMillis: Long): RxHttpRetry {
+        timeoutMillis = timeMillis
+        return this
+    }
+
+    fun retry(
+        times: Int = Int.MAX_VALUE,
+        period: Long = 0,
+        test: ((Throwable) -> Boolean)? = null
+    ): RxHttpRetry {
+        this.times = times
+        this.period = period
+        this.test = test
+        return this
+    }
+}
 
 /**
  * @param times  重试次数，默认Int.MAX_VALUE 代表不断重试
@@ -27,42 +49,59 @@ fun BaseRxHttp.retry(
     times: Int = Int.MAX_VALUE,
     period: Long = 0,
     test: ((Throwable) -> Boolean)? = null
-) = RxHttpRetry(this, times, period, test)
+) = RxHttpRetry(this).retry(times, period, test)
+
+fun BaseRxHttp.timeout(timeMillis: Long
+) = RxHttpRetry(this).timeout(timeMillis)
 
 suspend fun RxHttpRetry.awaitBitmap() = await(BitmapParser())
-
-suspend inline fun <reified T : Any> RxHttpRetry.await() = await(object : SimpleParser<T>() {})
-
-suspend inline fun <reified T : Any> RxHttpRetry.awaitList() = await<List<T>>()
-
-suspend inline fun <reified K : Any, reified V : Any> RxHttpRetry.awaitMap() = await<Map<K, V>>()
 
 suspend fun RxHttpRetry.awaitHeaders(): Headers = awaitOkResponse().headers()
 
 suspend fun RxHttpRetry.awaitOkResponse() = await(OkResponseParser())
 
-suspend fun <T> RxHttpRetry.await(parser: Parser<T>) = await(baseRxHttp.buildRequest(), parser)
+suspend inline fun <reified T : Any> RxHttpRetry.awaitList() = await<List<T>>()
+
+suspend inline fun <reified K : Any, reified V : Any> RxHttpRetry.awaitMap() = await<Map<K, V>>()
+
+suspend inline fun <reified T : Any> RxHttpRetry.await() = await(object : SimpleParser<T>() {})
+
+suspend fun <T> RxHttpRetry.await(parser: Parser<T>) = awaitRetry(baseRxHttp.buildRequest(), parser)
 
 /**
  * @param request [okhttp3.Request] 对象，用于构建 [okhttp3.Call]对象
  * @param parser 解析器
  */
-private suspend fun <T> RxHttpRetry.await(
+private suspend fun <T> RxHttpRetry.awaitRetry(
     request: Request,
     parser: Parser<T>
 ): T {
     return try {
-        HttpSender.newCall(request).await(parser)
+        if (timeoutMillis > 0)
+            awaitTimeout(request, parser, timeoutMillis)
+        else
+            await(request, parser)
     } catch (e: Throwable) {
-        //剩余次数
-        val remaining = times
+        val remaining = times  //剩余次数
         if (remaining != Int.MAX_VALUE) {
             times = remaining - 1
         }
         val pass = test?.invoke(e) ?: true
         if (remaining > 0 && pass) {
             delay(period)
-            await(request, parser)
+            awaitRetry(request, parser)
         } else throw e
     }
 }
+
+private suspend fun <T> awaitTimeout(
+    request: Request,
+    parser: Parser<T>,
+    timeMillis: Long
+) = withTimeout(timeMillis) { await(request, parser) }
+
+
+private suspend fun <T> await(
+    request: Request,
+    parser: Parser<T>
+) = HttpSender.newCall(request).await(parser)
