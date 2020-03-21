25ce3ad6becaa81aa406734e3d6f8cd28be3713d
liujingxing <18268800083@163.com>
2020-03-21 19:27:50 +0800

    增加超时/重试例子

---
+++ app/src/main/java/com/example/httpsender/MyViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.httpsender
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.rxLifeScope
+import com.example.httpsender.entity.Article
+import com.example.httpsender.entity.PageList
+import com.rxjava.rxlife.ScopeViewModel
+import com.rxjava.rxlife.lifeOnMain
+import io.reactivex.Observable
+import kotlinx.coroutines.TimeoutCancellationException
+import rxhttp.timeout
+import rxhttp.wrapper.param.RxHttp
+import rxhttp.wrapper.param.awaitResponse
+import java.util.concurrent.TimeUnit
+
+/**
+ * User: ljx
+ * Date: 2019-05-31
+ * Time: 21:50
+ */
+class MyViewModel(application: Application) : ScopeViewModel(application) {
+
+    fun startInterval() {
+        Observable.interval(1, 1, TimeUnit.SECONDS)
+            .lifeOnMain(this)
+            .subscribe { Log.e("LJX", "MyViewModel aLong=$it") }
+    }
+
+    fun testRetry() = rxLifeScope.launch({
+        val pageList = RxHttp.get("/article/list/0/json")
+            .timeout(100)
+            .retry(2, 1000) {
+                it is TimeoutCancellationException
+            }
+            .awaitResponse<PageList<Article>>()
+        Log.e("LJX", "pageList=$pageList")
+    }, {
+        Log.e("LJX", "it=$it")
+    })
+}
