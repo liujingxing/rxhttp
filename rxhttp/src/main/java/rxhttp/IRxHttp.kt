10bf76d9811a7b024b2a5536a1464f305e41ec23
liujingxing <18268800083ljx@gmail.com>
2020-04-15 22:20:37 +0800

    新增一系列AwaitXxx类

--- rxhttp/src/main/java/rxhttp/IRxHttp.kt
+++ rxhttp/src/main/java/rxhttp/IRxHttp.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.CoroutineScope
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import rxhttp.wrapper.await.AwaitImpl
+import rxhttp.wrapper.await.await
 import rxhttp.wrapper.callback.ProgressCallbackImpl
 import rxhttp.wrapper.entity.Progress
 import rxhttp.wrapper.parse.*
@@ -20,14 +22,6 @@ abstract class IRxHttp {
     //断点下载进度偏移量，进在带进度断点下载时生效
     open val breakDownloadOffSize = 0L
 
-    /**
-     * 失败重试/超时等，会重写此方法；扩展方法IRxHttp.awaitXxx，最终都会调用本方法
-     */
-    open suspend fun <T> await(
-        parser: Parser<T>,
-        client: OkHttpClient = HttpSender.getOkHttpClient(),
-        request: Request = buildRequest()
-    ) = HttpSender.newCall(client, request).await(parser)
 }
 
 suspend fun IRxHttp.awaitBoolean() = await<Boolean>()
@@ -73,3 +67,60 @@ suspend fun IRxHttp.awaitDownload(
     val clone = HttpSender.clone(ProgressCallbackImpl(coroutine, breakDownloadOffSize, progress))
     return await(DownloadParser(destPath), clone)
 }
+
+/**
+ * 以上await方法，最终都会调用本方法
+ */
+suspend fun <T> IRxHttp.await(
+    parser: Parser<T>,
+    client: OkHttpClient = HttpSender.getOkHttpClient()
+) = HttpSender.newCall(client, buildRequest()).await(parser)
+
+fun IRxHttp.toBoolean() = to<Boolean>()
+
+fun IRxHttp.toByte() = to<Byte>()
+
+fun IRxHttp.toShort() = to<Short>()
+
+fun IRxHttp.toInt() = to<Int>()
+
+fun IRxHttp.toLong() = to<Long>()
+
+fun IRxHttp.toFloat() = to<Float>()
+
+fun IRxHttp.toDouble() = to<Double>()
+
+fun IRxHttp.toStr() = to<String>()
+
+inline fun <reified T : Any> IRxHttp.toList() = to<List<T>>()
+
+inline fun <reified K : Any, reified V : Any> IRxHttp.toMap() = to<Map<K, V>>()
+
+fun IRxHttp.toBitmap() = to(BitmapParser())
+
+fun IRxHttp.toHeaders() = toOkResponse().map { it.headers() }
+
+fun IRxHttp.toOkResponse() = to(OkResponseParser())
+
+inline fun <reified T : Any> IRxHttp.to() = to(object : SimpleParser<T>() {})
+
+fun IRxHttp.toDownload(destPath: String) = to(DownloadParser(destPath))
+
+/**
+ * @param destPath 本地存储路径
+ * @param coroutine 用于开启一个协程，来控制进度回调所在的线程
+ * @param progress 进度回调
+ */
+fun IRxHttp.toDownload(
+    destPath: String,
+    coroutine: CoroutineScope? = null,
+    progress: (Progress) -> Unit
+): IAwait<String> {
+    val clone = HttpSender.clone(ProgressCallbackImpl(coroutine, breakDownloadOffSize, progress))
+    return to(DownloadParser(destPath), clone)
+}
+
+fun <T> IRxHttp.to(
+    parser: Parser<T>,
+    client: OkHttpClient = HttpSender.getOkHttpClient()
+): IAwait<T> = AwaitImpl(parser, buildRequest(), client)
