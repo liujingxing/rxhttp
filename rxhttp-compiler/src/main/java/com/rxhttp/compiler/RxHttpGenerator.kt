ec2834544dabf7bacc5ab41b77f17209643a5647
liujingxing <>
2020-04-19 14:57:53 +0800

    新增RxHttp.dispose(Disposable)、RxHttp.isDisposed(Disposable)方法

--- rxhttp-compiler/src/main/java/com/rxhttp/compiler/RxHttpGenerator.kt
+++ rxhttp-compiler/src/main/java/com/rxhttp/compiler/RxHttpGenerator.kt
@@ -157,6 +157,23 @@ class RxHttpGenerator {
                 .addStatement("return \$T.getOkHttpClient()", httpSenderName)
                 .returns(okHttpClientName).build())
 
+        if (isDependenceRxJava()) {
+            val disposableName = getClassName("Disposable")
+            methodList.add(
+                MethodSpec.methodBuilder("dispose")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .addParameter(disposableName, "disposable")
+                    .addStatement("if (!isDisposed(disposable)) disposable.dispose()")
+                    .build())
+
+            methodList.add(
+                MethodSpec.methodBuilder("isDisposed")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .addParameter(disposableName,"disposable")
+                    .addStatement("return disposable != null && disposable.isDisposed()")
+                    .returns(Boolean::class.java).build())
+        }
+
         methodList.add(
             MethodSpec.methodBuilder("getParam")
                 .addModifiers(Modifier.PUBLIC)
