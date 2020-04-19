ac110f7aba6d117efb234d6131e7319c610c6dc1
“liujingxing” <18268800083ljx@gmail.com>
2020-04-19 14:01:22 +0800

    删除asUpload、awaitUpload系列方法

--- app/build/generated/source/kapt/debug/rxhttp/wrapper/param/RxHttp.kt
+++ app/build/generated/source/kapt/debug/rxhttp/wrapper/param/RxHttp.kt
@@ -5,7 +5,6 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.functions.Consumer
 import kotlin.Any
-import kotlin.Deprecated
 import kotlin.String
 import kotlin.Unit
 import kotlin.coroutines.resume
@@ -18,20 +17,19 @@ import rxhttp.await
 import rxhttp.toParser
 import rxhttp.wrapper.callback.ProgressCallback
 import rxhttp.wrapper.entity.Progress
-import rxhttp.wrapper.parse.Parser
 import rxhttp.wrapper.parse.SimpleParser
 
 suspend fun <T> Observable<T>.await(): T = suspendCancellableCoroutine { continuation ->
-    val subscribe = subscribe({                      
-        continuation.resume(it)                     
-    }, {                                             
-        continuation.resumeWithException(it)        
-    })                                              
-                                                    
-    continuation.invokeOnCancellation {              
-        subscribe.dispose()                         
-    }                                               
-}                                                   
+    val subscribe = subscribe({
+        continuation.resume(it)
+    }, {
+        continuation.resumeWithException(it)
+    })
+
+    continuation.invokeOnCancellation {
+        subscribe.dispose()
+    }
+}
 
 fun BaseRxHttp.asDownload(
   destPath: String,
@@ -48,40 +46,18 @@ inline fun <reified T> BaseRxHttp.asClass() = asParser(object : SimpleParser<T>(
 inline fun <reified T : Any> BaseRxHttp.asResponse() = asParser(object: ResponseParser<T>() {})
 
 /**
- * 调用此方法监听上传进度                                                    
+ * 调用此方法监听上传进度
  * @param observeOnScheduler  用于控制下游回调所在线程(包括进度回调)
- * @param progress 进度回调                                      
+ * @param progress 进度回调
  */
 fun RxHttpFormParam.upload(observeOnScheduler: Scheduler? = null, progress: (Progress) -> Unit) =
     upload(Consumer{ progress(it) }, observeOnScheduler)
 
 /**
- * please use [upload] + asXxx method instead
- */
-@Deprecated("Will be removed in a future release")
-inline fun <reified T : Any> RxHttpFormParam.asUpload(observeOnScheduler: Scheduler? = null,
-    noinline progress: (Progress) -> Unit) = asUpload(object: SimpleParser<T>() {},
-    observeOnScheduler, progress)
-
-/**
- * please use [upload] + asXxx method instead
- */
-@Deprecated("Will be removed in a future release")
-fun <T : Any> RxHttpFormParam.asUpload(
-  parser: Parser<T>,
-  observeOnScheduler: Scheduler? = null,
-  progress: (Progress) -> Unit
-): Observable<T> = asUpload(parser, Consumer{ progress(it) }, observeOnScheduler)
-
-suspend inline fun <reified T : Any> IRxHttp.awaitResponse() = await(object: ResponseParser<T>() {})
-
-inline fun <reified T : Any> IRxHttp.toResponse() = toParser(object: ResponseParser<T>() {})
-
-/**
- * 调用此方法监听上传进度                                                    
+ * 调用此方法监听上传进度
  * @param coroutine  CoroutineScope对象，用于开启协程回调进度，进度回调所在线程取决于协程所在线程
- * @param progress 进度回调  
- * 注意：此方法仅在协程环境下才生效                                         
+ * @param progress 进度回调
+ * 注意：此方法仅在协程环境下才生效
  */
 fun RxHttpFormParam.upload(coroutine: CoroutineScope? = null, progress: (Progress) -> Unit):
     RxHttpFormParam {
@@ -92,23 +68,6 @@ fun RxHttpFormParam.upload(coroutine: CoroutineScope? = null, progress: (Progres
   return this
 }
 
-/**
- * please use [upload] + awaitXxx method instead
- */
-@Deprecated("Will be removed in a future release")
-suspend inline fun <reified T : Any> RxHttpFormParam.awaitUpload(coroutine: CoroutineScope? = null,
-    noinline progress: (Progress) -> Unit): T = awaitUpload(object: SimpleParser<T>() {}, coroutine,
-    progress)
+suspend inline fun <reified T : Any> IRxHttp.awaitResponse() = await(object: ResponseParser<T>() {})
 
-/**
- * please use [upload] + awaitXxx method instead
- */
-@Deprecated("Will be removed in a future release")
-suspend fun <T : Any> RxHttpFormParam.awaitUpload(
-  parser: Parser<T>,
-  coroutine: CoroutineScope? = null,
-  progress: (Progress) -> Unit
-): T {
-  upload(coroutine, progress)
-  return await(parser)
-}
+inline fun <reified T : Any> IRxHttp.toResponse() = toParser(object: ResponseParser<T>() {})
