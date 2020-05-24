2ae2798cb93aebe085cab223549db1b8ce4c1070
liujingxing <327744707@qq.com>
2020-05-24 21:43:30 +0800

    使用HttpsUtils类处理https问题

--- rxhttp/src/main/java/rxhttp/wrapper/ssl/X509TrustManagerImpl.java
+++ rxhttp/src/main/java/rxhttp/wrapper/ssl/X509TrustManagerImpl.java
@@ -6,10 +6,12 @@ import java.security.cert.X509Certificate;
 import javax.net.ssl.X509TrustManager;
 
 /**
+ * @deprecated please use {@link HttpsUtils} instead
  * User: ljx
  * Date: 2019/1/21
  * Time: 14:11
  */
+@Deprecated
 public class X509TrustManagerImpl implements X509TrustManager {
     @Override
     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
