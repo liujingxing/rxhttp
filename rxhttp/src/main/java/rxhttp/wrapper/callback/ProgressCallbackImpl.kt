0525c83f466b76b14b8d95d59c095056ca96172a
liujingxing <327744707@qq.com>
2020-07-04 19:14:14 +0800

    toDownload方法传入的进度回调，支持在suspend方法中回调

--- rxhttp/src/main/java/rxhttp/wrapper/callback/ProgressCallbackImpl.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/callback/ProgressCallbackImpl.kt
@@ -11,7 +11,6 @@ import rxhttp.wrapper.entity.Progress
  * Time: 00:48
  */
 internal class ProgressCallbackImpl(
-    private val coroutine: CoroutineScope? = null,  //协程，用于对进度回调切换线程
     private val offsetSize: Long,
     private val progress: (Progress) -> Unit
 ) : ProgressCallback {
@@ -29,6 +28,35 @@ internal class ProgressCallbackImpl(
             if (currentProgress <= lastProgress) return
             lastProgress = currentProgress
         }
-        coroutine?.launch { progress(p) } ?: progress(p)
+        progress(p)
+    }
+}
+
+/**
+ * 下载进度回调，回调在suspend方法中回调
+ * User: ljx
+ * Date: 2020/3/22
+ * Time: 00:48
+ */
+internal class SuspendProgressCallbackImpl(
+    private val coroutine: CoroutineScope,  //协程，用于对进度回调切换线程
+    private val offsetSize: Long,
+    private val progress: suspend (Progress) -> Unit
+) : ProgressCallback {
+
+    private var lastProgress = 0   //上次下载进度
+
+    override fun onProgress(progress: Int, currentSize: Long, totalSize: Long) {
+        //这里最多回调100次,仅在进度有更新时,才会回调
+        val p = Progress(progress, currentSize, totalSize)
+        if (offsetSize > 0) {
+            p.addCurrentSize(offsetSize)
+            p.addTotalSize(offsetSize)
+            p.updateProgress()
+            val currentProgress: Int = p.progress
+            if (currentProgress <= lastProgress) return
+            lastProgress = currentProgress
+        }
+        coroutine.launch { progress(p) }
     }
 }
