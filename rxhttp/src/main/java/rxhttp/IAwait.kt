09fd3d8eb3884aaae83edbcc7a4ee7a9dc31f616
liujingxing <327744707@qq.com>
2020-07-22 23:25:04 +0800

    添加注释

--- rxhttp/src/main/java/rxhttp/IAwait.kt
+++ rxhttp/src/main/java/rxhttp/IAwait.kt
@@ -28,70 +28,138 @@ fun <T> IAwait<T>.retry(
     test: (suspend (Throwable) -> Boolean)? = null
 ): IAwait<T> = AwaitRetry(this, times, period, test)
 
+/**
+ * 控制上游线程，可调用多次
+ * @param context 协程上下文
+ */
 fun <T> IAwait<T>.flowOn(context: CoroutineContext): IAwait<T> = AwaitFlowOn(this, context)
 
-fun <T> IAwait<out MutableList<T>>.insert(element: T) = insert(-1, element)
+/**
+ * 往集合尾部插入一条数据
+ */
+fun <T> IAwait<out MutableList<T>>.insert(
+    element: T
+): IAwait<MutableList<T>> = insert(-1, element)
 
+/**
+ * 往集合指定位置插入一条数据
+ */
 fun <T> IAwait<out MutableList<T>>.insert(
     index: Int,
     element: T
 ): IAwait<MutableList<T>> = AwaitInsert(this, index, element)
 
-fun <T> IAwait<out MutableList<T>>.insertAll(elements: Collection<T>) = insertAll(-1, elements)
+/**
+ *  往集合尾部插入多条数据
+ */
+fun <T> IAwait<out MutableList<T>>.insertAll(
+    elements: Collection<T>
+): IAwait<MutableList<T>> = insertAll(-1, elements)
 
+/**
+ * 往集合指定位置插入多条数据
+ */
 fun <T> IAwait<out MutableList<T>>.insertAll(
     index: Int,
     elements: Collection<T>
 ): IAwait<MutableList<T>> = AwaitInsertAll(this, index, elements)
 
+/**
+ * 集合去重，根据对象的哈希值去重
+ */
 fun <T> IAwait<out Iterable<T>>.distinct(): IAwait<List<T>> = AwaitDistinct(this)
 
+/**
+ * 集合去重，根据表达式返回值的哈希值去重
+ */
 fun <T, K> IAwait<out Iterable<T>>.distinctBy(selector: (T) -> K): IAwait<List<T>> = AwaitDistinctBy(this, selector)
 
+/**
+ * 顺序、排序对象需要继承Comparable接口，实现排序规则，返回原集合
+ */
 fun <T : Comparable<T>> IAwait<out MutableList<T>>.sort(): IAwait<MutableList<T>> = AwaitSort(this)
 
+/**
+ * 倒序、排序对象需要继承Comparable接口，实现排序规则，返回原集合
+ */
 fun <T : Comparable<T>> IAwait<out MutableList<T>>.sortDescending(): IAwait<MutableList<T>> = sortWith(reverseOrder())
 
+/**
+ * 顺序、多维度排序，返回原集合
+ */
 fun <T> IAwait<out MutableList<T>>.sortBy(
     vararg selectors: (T) -> Comparable<*>?
 ): IAwait<MutableList<T>> = sortWith(compareBy(*selectors))
 
-inline fun <T, R : Comparable<R>> IAwait<out MutableList<T>>.sortBy(
-    crossinline selector: (T) -> R?
+/**
+ * 顺序、单维度排序，返回原集合
+ */
+inline fun <T> IAwait<out MutableList<T>>.sortBy(
+    crossinline selector: (T) -> Comparable<*>?
 ): IAwait<MutableList<T>> = sortWith(compareBy(selector))
 
-inline fun <T, R : Comparable<R>> IAwait<out MutableList<T>>.sortByDescending(
-    crossinline selector: (T) -> R?
+/**
+ * 倒序、单维度排序，返回原集合
+ */
+inline fun <T> IAwait<out MutableList<T>>.sortByDescending(
+    crossinline selector: (T) -> Comparable<*>?
 ): IAwait<MutableList<T>> = sortWith(compareByDescending(selector))
 
+/**
+ * 顺序、根据传入的表达式返回值进行排序，返回原集合
+ */
 fun <T> IAwait<out MutableList<T>>.sortWith(
     comparator: (T, T) -> Int
 ): IAwait<MutableList<T>> = sortWith(Comparator { t1, t2 -> comparator(t1, t2) })
 
+/**
+ * 顺序、根据传入Comparator接口对象进行排序，返回原集合
+ */
 fun <T> IAwait<out MutableList<T>>.sortWith(
     comparator: Comparator<in T>
 ): IAwait<MutableList<T>> = AwaitSortWith(this, comparator)
 
+/**
+ * 顺序、排序对象需要继承Comparable接口，实现排序规则，返回新的集合
+ */
 fun <T : Comparable<T>> IAwait<out Iterable<T>>.sorted(): IAwait<List<T>> = AwaitSorted(this)
 
+/**
+ * 顺序、排序对象需要继承Comparable接口，实现排序规则，返回新的集合
+ */
 fun <T : Comparable<T>> IAwait<out Iterable<T>>.sortedDescending(): IAwait<List<T>> = sortedWith(reverseOrder())
 
+/**
+ * 顺序、多维度排序，返回新的集合
+ */
 fun <T> IAwait<out Iterable<T>>.sortedBy(
     vararg selectors: (T) -> Comparable<*>?
 ): IAwait<List<T>> = sortedWith(compareBy(*selectors))
 
-inline fun <T, R : Comparable<R>> IAwait<out Iterable<T>>.sortedBy(
-    crossinline selector: (T) -> R?
+/**
+ * 顺序、单维度排序，返回新的集合
+ */
+inline fun <T> IAwait<out Iterable<T>>.sortedBy(
+    crossinline selector: (T) -> Comparable<*>?
 ): IAwait<List<T>> = sortedWith(compareBy(selector))
 
-inline fun <T, R : Comparable<R>> IAwait<out Iterable<T>>.sortedByDescending(
-    crossinline selector: (T) -> R?
+/**
+ * 倒序、单维度排序，返回新的集合
+ */
+inline fun <T> IAwait<out Iterable<T>>.sortedByDescending(
+    crossinline selector: (T) -> Comparable<*>?
 ): IAwait<List<T>> = sortedWith(compareByDescending(selector))
 
+/**
+ * 顺序、根据传入的表达式返回值进行排序，返回新的集合
+ */
 fun <T> IAwait<out Iterable<T>>.sortedWith(
     comparator: (T, T) -> Int
 ): IAwait<List<T>> = sortedWith(Comparator { t1, t2 -> comparator(t1, t2) })
 
+/**
+ * 顺序、根据传入Comparator接口对象进行排序，返回新的集合
+ */
 fun <T> IAwait<out Iterable<T>>.sortedWith(
     comparator: Comparator<in T>
 ): IAwait<List<T>> = AwaitSortedWith(this, comparator)
