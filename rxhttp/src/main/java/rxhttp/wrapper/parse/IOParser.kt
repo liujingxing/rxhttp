ac13b8b85f318e8cd59414456f9248fdec567630
liujingxing <327744707@qq.com>
2020-09-08 22:56:05 +0800

    下载支持传入Uri对象

--- rxhttp/src/main/java/rxhttp/wrapper/parse/IOParser.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/parse/IOParser.kt
@@ -1,8 +1,11 @@
 package rxhttp.wrapper.parse
 
+import okhttp3.Response
+import okhttp3.ResponseBody
+import rxhttp.wrapper.OkHttpCompat
 import rxhttp.wrapper.callback.ProgressCallback
 import rxhttp.wrapper.utils.IOUtil
-import java.io.InputStream
+import java.io.IOException
 import java.io.OutputStream
 
 /**
@@ -10,29 +13,32 @@ import java.io.OutputStream
  * Date: 2020/9/4
  * Time: 14:34
  */
-abstract class IOParser<T> : Parser<T> {
+abstract class IOParser : Parser<String> {
 
     var callback: ProgressCallback? = null
 
-    private var lastProgress = 0
+}
 
-    protected fun write(
-        inStream: InputStream?,
-        outStream: OutputStream?,
-        contentLength: Long,
-        offsetSize: Long = 0
-    ) {
-        val newContentLength = contentLength + offsetSize
-        IOUtil.write(inStream, outStream) {
-            callback?.apply {
-                val currentSize = it + offsetSize
-                //当前进度 = 当前已读取的字节 / 总字节
-                val currentProgress = ((currentSize * 100f / newContentLength)).toInt()
-                if (currentProgress > lastProgress) {
-                    lastProgress = currentProgress
-                    onProgress(currentProgress, currentSize, newContentLength)
-                }
-            }
+@Throws(IOException::class)
+fun Response.writeTo(
+    body: ResponseBody,
+    os: OutputStream,
+    callback: ProgressCallback? = null
+) {
+    val contentLength = OkHttpCompat.getContentLength(this)
+    val offsetSize = OkHttpCompat.getDownloadOffSize(this)?.offSize ?: 0
+    val newContentLength = contentLength + offsetSize
+
+    var lastProgress = 0
+
+    IOUtil.write(body.byteStream(), os) {
+        if (callback == null) return@write
+        val currentSize = it + offsetSize
+        //当前进度 = 当前已读取的字节 / 总字节
+        val currentProgress = ((currentSize * 100f / newContentLength)).toInt()
+        if (currentProgress > lastProgress) {
+            lastProgress = currentProgress
+            callback.onProgress(currentProgress, currentSize, newContentLength)
         }
     }
 }
