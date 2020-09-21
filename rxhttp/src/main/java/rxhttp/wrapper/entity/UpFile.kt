714124dd115ba31e6e1e38b8af229e39f472edf8
liujingxing <327744707@qq.com>
2020-09-21 14:55:48 +0800

    UpFile 类 代码简化

--- rxhttp/src/main/java/rxhttp/wrapper/entity/UpFile.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/entity/UpFile.kt
@@ -1,48 +1,32 @@
-package rxhttp.wrapper.entity;
+package rxhttp.wrapper.entity
 
-import java.io.File;
-import java.net.URI;
+import java.io.File
 
 /**
  * User: ljx
  * Date: 2018/12/21
  * Time: 09:21
  */
-public class UpFile extends File {
+class UpFile(
+    val key: String,
+    private var fileName: String?,
+    val file: File
+) {
 
-    private String key;
-    private String value; //用于文件上传时对应的value值, 为空时,默认为文件名
+    constructor(key: String, path: String) : this(key, null, File(path))
+    constructor(key: String, file: File) : this(key, null, file)
+    constructor(key: String, fileName: String?, path: String) : this(key, fileName, File(path))
 
-    public UpFile(String key, String pathname) {
-        super(pathname);
-        this.key = key;
+    @Deprecated("", ReplaceWith("setFileName(fileName)"))
+    fun setValue(fileName: String?) {
+        setFileName(fileName)
     }
 
-    public UpFile(String key, String parent, String child) {
-        super(parent, child);
-        this.key = key;
+    fun setFileName(fileName: String?) {
+        this.fileName = fileName
     }
 
-    public UpFile(String key, File parent, String child) {
-        super(parent, child);
-        this.key = key;
-    }
-
-    public UpFile(String key, URI uri) {
-        super(uri);
-        this.key = key;
-    }
-
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getValue() {
-        return value == null ? getName() : value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+    fun getFileName(): String {
+        return fileName ?: file.name
     }
 }
