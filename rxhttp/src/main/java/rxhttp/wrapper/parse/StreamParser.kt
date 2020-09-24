3a921e7f746754ce0fa9c8a90a7c53c226f822f4
liujingxing <327744707@qq.com>
2020-09-24 23:18:04 +0800

    修复偶现下载到100%时，100%进度不会调问题(原因：精度丢失)

--- rxhttp/src/main/java/rxhttp/wrapper/parse/StreamParser.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/parse/StreamParser.kt
@@ -5,7 +5,8 @@ import android.net.Uri
 import okhttp3.Response
 import okhttp3.ResponseBody
 import rxhttp.wrapper.OkHttpCompat
-import rxhttp.wrapper.callback.*
+import rxhttp.wrapper.callback.OutputStreamFactory
+import rxhttp.wrapper.callback.ProgressCallback
 import rxhttp.wrapper.callback.newOutputStreamFactory
 import rxhttp.wrapper.entity.Progress
 import rxhttp.wrapper.exception.ExceptionHelper
@@ -64,7 +65,7 @@ private fun Response.writeTo(
     IOUtil.write(body.byteStream(), os) {
         val currentSize = it + offsetSize
         //当前进度 = 当前已读取的字节 / 总字节
-        val currentProgress = ((currentSize * 100f / contentLength)).toInt()
+        val currentProgress = ((currentSize * 100 / contentLength)).toInt()
         if (currentProgress > lastProgress) {
             lastProgress = currentProgress
             val progress = Progress(currentProgress, currentSize, contentLength)
