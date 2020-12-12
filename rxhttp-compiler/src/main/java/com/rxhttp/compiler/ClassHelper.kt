1106ec5b2d237c1613a479d5c0073fbb5947b21e
liujingxing <327744707@qq.com>
2020-12-12 23:47:38 +0800

    新增addQuery、setQuery等一系列与查询参数相关的方法

--- rxhttp-compiler/src/main/java/com/rxhttp/compiler/ClassHelper.kt
+++ rxhttp-compiler/src/main/java/com/rxhttp/compiler/ClassHelper.kt
@@ -1041,16 +1041,6 @@ object ClassHelper {
                     return this;
                 }
 
-                public RxHttpNoBodyParam removeAllBody() {
-                    param.removeAllBody();
-                    return this;
-                }
-
-                public RxHttpNoBodyParam removeAllBody(String key) {
-                    param.removeAllBody(key);
-                    return this;
-                }
-
                 public RxHttpNoBodyParam set(String key, Object value) {
                     param.set(key, value);
                     return this;
@@ -1059,14 +1049,6 @@ object ClassHelper {
                 public RxHttpNoBodyParam setEncoded(String key, Object value) {
                     param.setEncoded(key, value);
                     return this;
-                }
-
-                public Object queryValue(String key) {
-                    return param.queryValue(key);
-                }
-
-                public List<Object> queryValues(String key) {
-                    return param.queryValues(key);
                 }
             }
 
--- rxhttp-compiler/src/main/java/com/rxhttp/compiler/ParamsAnnotatedClass.kt
+++ rxhttp-compiler/src/main/java/com/rxhttp/compiler/ParamsAnnotatedClass.kt
@@ -237,6 +237,99 @@ class ParamsAnnotatedClass {
                 .build())
 
         methodList.add(
+            MethodSpec.methodBuilder("removeAllQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addStatement("param.removeAllQuery()")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("removeAllQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String::class.java, "key")
+                .addStatement("param.removeAllQuery(key)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("addQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String::class.java, "key")
+                .addParameter(Any::class.java, "value")
+                .addStatement("param.addQuery(key,value)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("setQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String::class.java, "key")
+                .addParameter(Any::class.java, "value")
+                .addStatement("param.setQuery(key,value)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("addEncodedQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String::class.java, "key")
+                .addParameter(Any::class.java, "value")
+                .addStatement("param.addEncodedQuery(key,value)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("setEncodedQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String::class.java, "key")
+                .addParameter(Any::class.java, "value")
+                .addStatement("param.setEncodedQuery(key,value)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("addAllQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(mapName, "map")
+                .addStatement("param.addAllQuery(map)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("setAllQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(mapName, "map")
+                .addStatement("param.setAllQuery(map)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("addAllEncodedQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(mapName, "map")
+                .addStatement("param.addAllEncodedQuery(map)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
+            MethodSpec.methodBuilder("setAllEncodedQuery")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(mapName, "map")
+                .addStatement("param.setAllEncodedQuery(map)")
+                .addStatement("return (R)this")
+                .returns(rxHttp)
+                .build())
+
+        methodList.add(
             MethodSpec.methodBuilder("addHeader")
                 .addModifiers(Modifier.PUBLIC)
                 .addParameter(String::class.java, "line")
