b5f8bbc186fac14f2862389d1fa756fa30c2381c
liujingxing <327744707@qq.com>
2021-03-14 12:17:46 +0800

    修复RxHttpJsonParam#addAll(String)方法，对于超大整型、浮点型数据，序列化过程精度丢失问题 close #225

--- rxhttp/src/main/java/rxhttp/wrapper/utils/Json.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/utils/Json.kt
@@ -6,6 +6,7 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
+import com.google.gson.internal.LazilyParsedNumber
 import java.util.*
 
 /**
@@ -13,6 +14,16 @@ import java.util.*
  * Date: 2020/8/1
  * Time: 17:27
  */
+
+fun JsonElement.toAny(): Any? {
+    return when (this) {
+        is JsonObject -> toMap()
+        is JsonArray -> toList()
+        is JsonPrimitive -> toAny()
+        else -> null
+    }
+}
+
 fun JsonObject.toMap(): Map<String, Any?> {
     val map: MutableMap<String, Any?> = LinkedHashMap()
     for ((key, value) in entrySet()) {
@@ -27,25 +38,35 @@ fun JsonArray.toList(): List<Any?> {
     return list
 }
 
-fun JsonElement.toAny(): Any? {
-    return when (this) {
-        is JsonObject -> toMap()
-        is JsonArray -> toList()
-        is JsonPrimitive -> toAny()
-        else -> null
-    }
-}
-
 fun JsonPrimitive.toAny(): Any {
     return when {
-        isNumber -> asNumber.toAny()
+        isNumber -> asNumber.toNumber()
         isBoolean -> asBoolean
         else -> asString
     }
 }
 
-fun Number.toAny(): Any {
-    val d = toDouble()
-    val i = d.toLong()
-    return if (d == i.toDouble() && i.toString().length == toString().length) i else d
+/**
+ * 因fastJson、moshi等第三方数据解析工具正常情况下无法对LazilyParsedNumber类型数据，做出正确的序列化操作
+ * 故对于正常数据，转为基本类型，对于超大整型、浮点型数据，转为BigInteger、BigDecimal (moshi 需要额外配置，才能对这两个数据类型正常序列化)
+ */
+fun Number.toNumber(): Number {
+    return if (this is LazilyParsedNumber) {
+        val number = toString()
+        if (number.contains(".")) {
+            val double = toDouble()
+            if (double.toString() == number) {
+                double
+            } else {
+                number.toBigDecimal()
+            }
+        } else {
+            val long = toLong()
+            if (long.toString() == number) {
+                long
+            } else {
+                number.toBigInteger()
+            }
+        }
+    } else this
 }
