263b08c5bc4dbf96a58f524b6d5f94736f9164ac
liujingxing <327744707@qq.com>
2021-03-19 18:15:27 +0800

    简化代码

--- app/src/main/java/com/example/httpsender/kt/KotlinExtensions.kt
+++ app/src/main/java/com/example/httpsender/kt/KotlinExtensions.kt
@@ -3,9 +3,9 @@ package com.example.httpsender.kt
 import android.content.Context
 import android.net.ConnectivityManager
 import com.example.httpsender.AppHolder
-import com.example.httpsender.R
 import com.example.httpsender.Tip
 import com.google.gson.JsonSyntaxException
+import kotlinx.coroutines.TimeoutCancellationException
 import rxhttp.wrapper.exception.HttpStatusCodeException
 import rxhttp.wrapper.exception.ParseException
 import java.net.ConnectException
@@ -29,15 +29,9 @@ fun String.show() {
 val Throwable.errorCode: Int
     get() {
         val errorCode = when (this) {
-            is HttpStatusCodeException -> {//请求失败异常
-                this.statusCode
-            }
-            is ParseException -> {  // ParseException异常表明请求成功，但是数据不正确
-                this.errorCode
-            }
-            else -> {
-                "-1"
-            }
+            is HttpStatusCodeException -> this.statusCode //Http状态码异常
+            is ParseException -> this.errorCode     //业务code异常
+            else -> "-1"
         }
         return try {
             errorCode.toInt()
@@ -46,37 +40,31 @@ val Throwable.errorCode: Int
         }
     }
 
-
 val Throwable.errorMsg: String
     get() {
-        var errorMsg = handleNetworkException(this)  //网络异常
-        if (this is HttpStatusCodeException) {               //请求失败异常
-            val code = this.statusCode
-            if ("416" == code) {
-                errorMsg = "请求范围不符合要求"
-            }
+        return if (this is UnknownHostException) { //网络异常
+            if (!isNetworkConnected(AppHolder.getInstance()))
+                "当前无网络，请检查你的网络设置"
+            else
+                "网络连接不可用，请稍后重试！"
+        } else if (
+            this is SocketTimeoutException  //okhttp全局设置超时
+            || this is TimeoutException     //rxjava中的timeout方法超时
+            || this is TimeoutCancellationException  //协程超时
+        ) {
+            "连接超时,请稍后再试"
+        } else if (this is ConnectException) {
+            "网络不给力，请稍候重试！"
+        } else if (this is HttpStatusCodeException) {               //请求失败异常
+            "Http状态码异常"
         } else if (this is JsonSyntaxException) {  //请求成功，但Json语法异常,导致解析失败
-            errorMsg = "数据解析失败,请稍后再试"
+            "数据解析失败,请检查数据是否正确"
         } else if (this is ParseException) {       // ParseException异常表明请求成功，但是数据不正确
-            errorMsg = this.message ?: errorCode   //errorMsg为空，显示errorCode
-        }
-        return errorMsg ?: message ?: this.toString()
-    }
-
-//处理网络异常
-private fun <T> handleNetworkException(throwable: T): String? {
-    val stringId =
-        if (throwable is UnknownHostException) { //网络异常
-            if (!isNetworkConnected(AppHolder.getInstance())) R.string.network_error else R.string.notify_no_network
-        } else if (throwable is SocketTimeoutException || throwable is TimeoutException) {
-            R.string.time_out_please_try_again_later  //前者是通过OkHttpClient设置的超时引发的异常，后者是对单个请求调用timeout方法引发的超时异常
-        } else if (throwable is ConnectException) {
-            R.string.esky_service_exception  //连接异常
+            this.message ?: errorCode   //msg为空，显示code
         } else {
-            -1
+            message ?: this.toString()
         }
-    return if (stringId == -1) null else AppHolder.getInstance().getString(stringId)
-}
+    }
 
 private fun isNetworkConnected(context: Context): Boolean {
     val mConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
