1290094b72a60d32cc4f1484488d65f3078f43c1
liujingxing <327744707@qq.com>
2021-05-08 14:55:42 +0800

    更改注释

--- app/build/generated/source/kapt/debug/rxhttp/wrapper/param/BaseRxHttp.java
+++ app/build/generated/source/kapt/debug/rxhttp/wrapper/param/BaseRxHttp.java
@@ -31,7 +31,8 @@ import rxhttp.wrapper.utils.LogUtil;
 import rxhttp.wrapper.utils.UriUtil;
 
 /**
- * 本类存放asXxx方法，如果依赖了RxJava的话
+ * 本类存放asXxx方法(需要单独依赖RxJava，并告知RxHttp依赖的RxJava版本)
+ * 如未生成，请查看 https://github.com/liujingxing/okhttp-RxHttp/wiki/FAQ
  * User: ljx
  * Date: 2020/4/11
  * Time: 18:15
