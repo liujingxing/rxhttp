1edf47fa54a3f03f2d8916110538a36977b125de
liujingxing <327744707@qq.com>
2021-05-13 14:43:07 +0800

    代码简化

--- rxhttp/src/main/java/rxhttp/wrapper/entity/ParameterizedTypeImpl.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/entity/ParameterizedTypeImpl.kt
@@ -2,7 +2,6 @@ package rxhttp.wrapper.entity
 
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
-import java.util.*
 
 /**
  * User: ljx
@@ -15,7 +14,6 @@ class ParameterizedTypeImpl(
     private vararg val actualTypeArguments: Type
 ) : ParameterizedType {
 
-    //适用于单个泛型参数的类
     constructor(rawType: Type, actualType: Type) : this(null, rawType, actualType)
 
     override fun getActualTypeArguments(): Array<out Type> {
@@ -42,20 +40,17 @@ class ParameterizedTypeImpl(
          */
         @JvmStatic
         operator fun get(rawType: Type, vararg types: Type): ParameterizedTypeImpl {
-            val length = types.size
-            if (length > 1) {
-                val parameterizedType: Type = ParameterizedTypeImpl(types[length - 2], types[length - 1])
-                val newTypes = Arrays.copyOf(types, length - 1)
-                newTypes[newTypes.size - 1] = parameterizedType
-                return get(rawType, *newTypes)
+            val size = types.size
+            var lastType = types[size - 1]
+            for (i in size - 2 downTo 0) { //The tail starts traversing
+                lastType = ParameterizedTypeImpl(types[i], lastType)
             }
-            return ParameterizedTypeImpl(rawType, types[0])
+            return ParameterizedTypeImpl(rawType, lastType)
         }
 
         //适用于多个泛型参数的类
         @JvmStatic
-        fun getParameterized(rawType: Type, vararg actualTypeArguments: Type): ParameterizedTypeImpl {
-            return ParameterizedTypeImpl(null, rawType, *actualTypeArguments)
-        }
+        fun getParameterized(rawType: Type, vararg actualTypeArguments: Type) =
+            ParameterizedTypeImpl(null, rawType, *actualTypeArguments)
     }
 }
