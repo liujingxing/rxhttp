aa18b53b6eb2bd6dd2d91481bc479a2e24ca4947
liujingxing <327744707@qq.com>
2021-06-13 22:15:24 +0800

    kotlinpoet 升级至1.8.0版本

--- app/build/generated/source/kapt/debug/rxhttp/wrapper/param/RxHttp.kt
+++ app/build/generated/source/kapt/debug/rxhttp/wrapper/param/RxHttp.kt
@@ -1,4 +1,4 @@
-package rxhttp.wrapper.param
+package rxhttp.wrapper.`param`
 
 import com.example.httpsender.parser.ResponseParser
 import kotlin.Any
@@ -10,24 +10,25 @@ import rxhttp.toParser
 import rxhttp.wrapper.entity.Progress
 import rxhttp.wrapper.parse.SimpleParser
 
-inline fun <reified T> RxHttp<*, *>.executeList() = executeClass<List<T>>()
+public inline fun <reified T> RxHttp<*, *>.executeList() = executeClass<List<T>>()
 
-inline fun <reified T> RxHttp<*, *>.executeClass() = execute(object : SimpleParser<T>() {})
+public inline fun <reified T> RxHttp<*, *>.executeClass() = execute(object : SimpleParser<T>() {})
 
-inline fun <reified T> BaseRxHttp.asList() = asClass<List<T>>()
+public inline fun <reified T> BaseRxHttp.asList() = asClass<List<T>>()
 
-inline fun <reified K, reified V> BaseRxHttp.asMap() = asClass<Map<K,V>>()
+public inline fun <reified K, reified V> BaseRxHttp.asMap() = asClass<Map<K,V>>()
 
-inline fun <reified T> BaseRxHttp.asClass() = asParser(object : SimpleParser<T>() {})
+public inline fun <reified T> BaseRxHttp.asClass() = asParser(object : SimpleParser<T>() {})
 
-inline fun <reified T : Any> BaseRxHttp.asResponse() = asParser(object: ResponseParser<T>() {})
+public inline fun <reified T : Any> BaseRxHttp.asResponse() = asParser(object: ResponseParser<T>()
+    {})
 
 /**
  * 调用此方法监听上传进度                                                    
  * @param coroutine  CoroutineScope对象，用于开启协程回调进度，进度回调所在线程取决于协程所在线程
  * @param progress 进度回调  
  */
-fun <P : AbstractBodyParam<P>, R : RxHttpAbstractBodyParam<P, R>> RxHttpAbstractBodyParam<P,
+public fun <P : AbstractBodyParam<P>, R : RxHttpAbstractBodyParam<P, R>> RxHttpAbstractBodyParam<P,
     R>.upload(coroutine: CoroutineScope, progress: suspend (Progress) -> Unit): R {
   param.setProgressCallback {
       coroutine.launch { progress(it) }
@@ -36,4 +37,4 @@ fun <P : AbstractBodyParam<P>, R : RxHttpAbstractBodyParam<P, R>> RxHttpAbstract
   return this as R
 }
 
-inline fun <reified T : Any> IRxHttp.toResponse() = toParser(object: ResponseParser<T>() {})
+public inline fun <reified T : Any> IRxHttp.toResponse() = toParser(object: ResponseParser<T>() {})
