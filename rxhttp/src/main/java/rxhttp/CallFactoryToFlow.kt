5932c62d887b4093f4312f1fca91dce3cab3a94a
liujingxing <327744707@qq.com>
2021-09-18 17:38:05 +0800

    toXxx/ToFlowXxx方法移动至单独的文件中

---
+++ rxhttp/src/main/java/rxhttp/CallFactoryToFlow.kt
@@ -0,0 +1,99 @@
+package rxhttp
+
+import android.content.Context
+import android.net.Uri
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.*
+import rxhttp.wrapper.callback.FileOutputStreamFactory
+import rxhttp.wrapper.callback.OutputStreamFactory
+import rxhttp.wrapper.callback.UriOutputStreamFactory
+import rxhttp.wrapper.entity.Progress
+import rxhttp.wrapper.entity.ProgressT
+
+/**
+ * CallFactory convert Flow
+ *
+ * User: ljx
+ * Date: 2021/9/18
+ * Time: 17:31
+ */
+inline fun <reified T : Any> CallFactory.toFlow(iAwait: IAwait<T> = toClass()): Flow<T> =
+    flow { emit(iAwait.await()) }
+
+@ExperimentalCoroutinesApi
+inline fun <reified T : Any> BodyParamFactory.toFlow(
+    noinline progress: suspend (Progress) -> Unit
+) = toFlowProgress<T>().onEachProgress(progress)
+
+@ExperimentalCoroutinesApi
+inline fun <reified T : Any> BodyParamFactory.toFlowProgress(
+    iAwait: IAwait<T> = toClass()
+) =
+    channelFlow {
+        param.setProgressCallback { progress, currentSize, totalSize ->
+            trySend(ProgressT<T>(progress, currentSize, totalSize))
+        }
+        iAwait.await().also { trySend(ProgressT<T>(it)) }
+    }.buffer(1, BufferOverflow.DROP_OLDEST)
+
+/**
+ * @param destPath Local storage path
+ * @param append is append download
+ * @param progress Progress callback in suspend method, The callback thread depends on the coroutine thread
+ */
+fun CallFactory.toFlow(
+    destPath: String,
+    append: Boolean = false,
+    progress: (suspend (Progress) -> Unit)? = null
+): Flow<String> = toFlow(FileOutputStreamFactory(destPath), append, progress)
+
+fun CallFactory.toFlow(
+    context: Context,
+    uri: Uri,
+    append: Boolean = false,
+    progress: (suspend (Progress) -> Unit)? = null
+): Flow<Uri> = toFlow(UriOutputStreamFactory(context, uri), append, progress)
+
+fun <T> CallFactory.toFlow(
+    osFactory: OutputStreamFactory<T>,
+    append: Boolean = false,
+    progress: (suspend (Progress) -> Unit)? = null
+): Flow<T> =
+    if (progress == null) {
+        flow {
+            setRangeHeader(osFactory, append)
+            emit(toSyncDownload(osFactory).await())
+        }.flowOn(Dispatchers.IO)
+    } else {
+        toFlowProgress(osFactory, append)
+            .onEachProgress(progress)
+    }
+
+fun CallFactory.toFlowProgress(
+    destPath: String,
+    append: Boolean = false
+): Flow<ProgressT<String>> = toFlowProgress(FileOutputStreamFactory(destPath), append)
+
+fun CallFactory.toFlowProgress(
+    context: Context,
+    uri: Uri,
+    append: Boolean = false
+): Flow<ProgressT<Uri>> = toFlowProgress(UriOutputStreamFactory(context, uri), append)
+
+fun <T> CallFactory.toFlowProgress(
+    osFactory: OutputStreamFactory<T>,
+    append: Boolean = false
+): Flow<ProgressT<T>> =
+    flow {
+        setRangeHeader(osFactory, append)
+        toSyncDownload(osFactory) { emit(it) }
+            .await().let { emit(ProgressT(it)) }
+    }
+        .buffer(1, BufferOverflow.DROP_OLDEST)
+        .flowOn(Dispatchers.IO)
+
+fun <T> Flow<ProgressT<T>>.onEachProgress(progress: suspend (Progress) -> Unit): Flow<T> =
+    onEach { if (it.result == null) progress(it) }
+        .mapNotNull { it.result }
