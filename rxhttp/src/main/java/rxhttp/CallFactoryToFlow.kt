7858f791492ca425996b25091fd07d68bfa8233a
liujingxing <327744707@qq.com>
2021-09-25 13:44:01 +0800

    toFlow/toFlowProgress/toDownload方法增加capacity参数，通过该参数，可配置队列缓存大小

--- rxhttp/src/main/java/rxhttp/CallFactoryToFlow.kt
+++ rxhttp/src/main/java/rxhttp/CallFactoryToFlow.kt
@@ -47,24 +47,28 @@ inline fun <reified T : Any> BodyParamFactory.toFlowProgress(
 /**
  * @param destPath Local storage path
  * @param append is append download
+ * @param capacity capacity of the buffer between coroutines
  * @param progress Progress callback in suspend method, The callback thread depends on the coroutine thread
  */
 fun CallFactory.toFlow(
     destPath: String,
     append: Boolean = false,
+    capacity: Int = 1,
     progress: (suspend (Progress) -> Unit)? = null
-): Flow<String> = toFlow(FileOutputStreamFactory(destPath), append, progress)
+): Flow<String> = toFlow(FileOutputStreamFactory(destPath), append, capacity, progress)
 
 fun CallFactory.toFlow(
     context: Context,
     uri: Uri,
     append: Boolean = false,
+    capacity: Int = 1,
     progress: (suspend (Progress) -> Unit)? = null
-): Flow<Uri> = toFlow(UriOutputStreamFactory(context, uri), append, progress)
+): Flow<Uri> = toFlow(UriOutputStreamFactory(context, uri), append, capacity, progress)
 
 fun <T> CallFactory.toFlow(
     osFactory: OutputStreamFactory<T>,
     append: Boolean = false,
+    capacity: Int = 1,
     progress: (suspend (Progress) -> Unit)? = null
 ): Flow<T> =
     if (progress == null) {
@@ -73,31 +77,34 @@ fun <T> CallFactory.toFlow(
             emit(toSyncDownload(osFactory).await())
         }.flowOn(Dispatchers.IO)
     } else {
-        toFlowProgress(osFactory, append)
+        toFlowProgress(osFactory, append, capacity)
             .onEachProgress(progress)
     }
 
 fun CallFactory.toFlowProgress(
     destPath: String,
-    append: Boolean = false
-): Flow<ProgressT<String>> = toFlowProgress(FileOutputStreamFactory(destPath), append)
+    append: Boolean = false,
+    capacity: Int = 1
+): Flow<ProgressT<String>> = toFlowProgress(FileOutputStreamFactory(destPath), append, capacity)
 
 fun CallFactory.toFlowProgress(
     context: Context,
     uri: Uri,
-    append: Boolean = false
-): Flow<ProgressT<Uri>> = toFlowProgress(UriOutputStreamFactory(context, uri), append)
+    append: Boolean = false,
+    capacity: Int = 1
+): Flow<ProgressT<Uri>> = toFlowProgress(UriOutputStreamFactory(context, uri), append, capacity)
 
 fun <T> CallFactory.toFlowProgress(
     osFactory: OutputStreamFactory<T>,
-    append: Boolean = false
+    append: Boolean = false,
+    capacity: Int = 1
 ): Flow<ProgressT<T>> =
     flow {
         setRangeHeader(osFactory, append)
         toSyncDownload(osFactory) { emit(it) }
             .await().let { emit(ProgressT(it)) }
     }
-        .buffer(1, BufferOverflow.DROP_OLDEST)
+        .buffer(capacity, BufferOverflow.DROP_OLDEST)
         .flowOn(Dispatchers.IO)
 
 fun <T> Flow<ProgressT<T>>.onEachProgress(progress: suspend (Progress) -> Unit): Flow<T> =
