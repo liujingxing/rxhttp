8375c272099a41aa0c7bd89218b285c8d318e9e2
liujingxing <327744707@qq.com>
2021-10-24 12:43:18 +0800

    LogLogInterceptor构造方法增加CookieJar参数

--- rxhttp/src/main/java/rxhttp/wrapper/intercept/LogInterceptor.kt
+++ rxhttp/src/main/java/rxhttp/wrapper/intercept/LogInterceptor.kt
@@ -1,8 +1,8 @@
 package rxhttp.wrapper.intercept
 
+import okhttp3.CookieJar
 import okhttp3.Interceptor
 import okhttp3.Response
-import okhttp3.internal.connection.RealCall
 import rxhttp.wrapper.utils.LogTime
 import rxhttp.wrapper.utils.LogUtil
 
@@ -14,13 +14,12 @@ import rxhttp.wrapper.utils.LogUtil
  * Date: 2021/10/17
  * Time: 16:42
  */
-class LogInterceptor : Interceptor {
+class LogInterceptor(private val cookieJar: CookieJar) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val okHttpClient = (chain.call() as RealCall).client
         var request = chain.request()
         // Prints the request start log
-        LogUtil.log(request, okHttpClient.cookieJar)
+        LogUtil.log(request, cookieJar)
         // Record the request start time
         request = request.newBuilder()
             .tag(LogTime::class.java, LogTime())
